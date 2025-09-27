cddd0637973c952ff228083841bc70c6330125ce
ZeusA11Hasan <althafhasan03@gmail.com>
2025-09-27 11:43:06 +0530

    Fix npm and package.json errors

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5055',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
+    host: true,
+    port: 5173
   }
 })
